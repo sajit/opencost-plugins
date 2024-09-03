97de062ea991eafa918367d4be98f6d79925b651
Alex Meijer <ameijer@kubecost.com>
2024-09-03 13:40:21 -0400

    try out workflow

--- datadog/datadogplugin/datadogconfig.go
+++
@@ -1,8 +0,0 @@
-package datadog
-
-type DatadogConfig struct {
-	DDSite     string `json:"datadog_site"`
-	DDAPIKey   string `json:"datadog_api_key"`
-	DDAppKey   string `json:"datadog_app_key"`
-	DDLogLevel string `json:"log_level"`
-}
